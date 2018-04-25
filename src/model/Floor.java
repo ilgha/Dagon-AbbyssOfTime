46ec13f1a2b9cc7556e9913dc680287b427cca74
Gaspard <Gaspard@fullmetal-pc.home>
2018-04-25 22:53:41 +0200

    A faire:
    -Déplacement par le game
    
    -Projectiles
    
    -Ennemies qui bougent
    
    -Map (enchainement tableau)
    
    
    DeadLine Dimanche

--- src/model/Floor.java
+++ src/model/Floor.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Floor {
+	private ArrayList<Room> rooms = new ArrayList<Room>();
 
 }
--- src/model/Room.java
+++ src/model/Room.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Room {
+	private ArrayList<Door> doors = new ArrayList<Door>();
 
 }
